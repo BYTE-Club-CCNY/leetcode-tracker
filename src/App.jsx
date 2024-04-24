fb67daa12050ea1179347860d426e5d768e95ebb
silvertoothGeek0 <bbvvleonardwolf@gmail.com>
2024-04-24 16:18:46 -0400

    working with persistent sessions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import { Router } from 'react-chrome-extension-router'; 
 import Landing from './pages/landing';
+import Home from './pages/home';
+import supabase from './supabaseClient';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [session, setSession] = useState(null);
+  
+  useEffect(() => {
+    (async () => {
+      let {data, error} = await supabase.auth.getSession();
+      !error ? setSession(data.session) : setSession(null);
+    })();
+  }, [])
+
   return (
     <Router>
-      <Landing />
+      {session ? <Home leetUser={session.user.user_metadata.leetcodeUser}/> : <Landing />}
     </Router>
   );
 };
